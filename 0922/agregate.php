34d7aa38d72f25c05cfba8024c9f2c0c0e1d91fa
artistic software <artisticsoftware41@gmail.com>
2024-10-21 09:39:26 +0000

    ...

---
+++ 0922/agregate.php
@@ -0,0 +1,79 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 2, 7, 9',
+		'place(end-favorites )' => '9',
+		'1 won(place bet)' => '140',
+		'places($10)' => '9',
+		'sures($10)' => '9',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1, 4, 6, 8',
+		'place(end-favorites )' => '8',
+		'places($10)' => '8',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 4, 5',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 1, 5',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 2',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 1, 3, 4, 8',
+		'place(end-favorites )' => '8',
+		'1 won(place bet)' => '195',
+		'places($10)' => '8',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 1, 6, 7',
+		'place(end-favorites )' => '7',
+		'places($10)' => '7',
+		'sures($10)' => '7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 3, 7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 1, 4, 5, 9',
+		'place(end-favorites )' => '9',
+		'1 won(place bet)' => '220',
+		'places($10)' => '9',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 2, 5, 6, 10, 11',
+	],
+];
---
+++ 0922/bets122.php
@@ -0,0 +1,212 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '9',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',//count diff: 10
+		],
+		'win inter' => '1, 3, 5, 7, 8',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '9',//count wp: 1
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',//count diff: 10
+		],
+		'win inter' => '1, 2, 6, 8, 9',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '1',//count wp: 1
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',//count diff: 11
+		],
+		'win inter' => '1, 2, 3, 4, 6, 9, 10',
+		'allValues' => '1, 2, 3, 5, 6, 7',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '5',//count wp: 1
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13',//count diff: 13
+		],
+		'win inter' => '1, 3, 4, 5, 6, 8, 10, 11, 13',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '1',//count wp: 1
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',//count diff: 14
+		],
+		'win inter' => '1, 4, 8, 11',
+		'allValues' => '1, 2, 3, 4, 5, 10, 11',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '2',//count wp: 1
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',//count diff: 11
+		],
+		'win inter' => '1, 2, 4, 6, 8, 9, 11',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '4',//count wp: 1
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '6',
+		'runners' => '1, 2, 3, 4, 5, 6, 7',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7',//count diff: 7
+		],
+		'win inter' => '1, 2, 3, 6',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '6',//count wp: 1
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '3',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14',//count diff: 14
+		],
+		'win inter' => '3, 4, 5, 6, 7, 8',
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '3',//count wp: 1
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1, 5',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'suggestions' => [
+			'win' => '1, 5, 8',
+			'qin' => '1, 3, 5, 7, 8, 9',
+			'trio' => '1, 2, 3, 5, 6, 7, 8, 9',//count trio: 8
+			'inter' => '1, 5',
+			'diff' => '4, 10',//count diff: 2
+		],
+		'win inter' => '1, 3, 8, 9',
+		'win inter 2' => '1, 5, 8',//count win inter 2: 3
+		'allValues' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '1, 5',//count wp: 2
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '2',
+		'runners' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',
+		'suggestions' => [
+			'win' => '',
+			'qin' => '',
+			'trio' => '',//count trio: 0
+			'inter' => '',
+			'diff' => '1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11',//count diff: 11
+		],
+		'win inter' => '1, 3, 7, 10',
+		'allValues' => '',
+		'bets' => [
+		],
+		'total bets' => 0,
+		'wp' => '',//count wp: 0
+	],
+];
+//total place end favorites: 0
+//total place end wp: 0
+//total place wp: 0
+//total sure place: 0
+//total win: 0
+//total place union: 0
+//total: 0
