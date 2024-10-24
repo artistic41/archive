05841a22fd5b2e17ef5fa97fd4c07a2bcd65f248
artistic software <artisticsoftware41@gmail.com>
2024-10-24 02:43:03 +0000

    ...

---
+++ 1023/agregate.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 1, 4, 5, 9, 11',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 3, 4, 5, 8, 11, 12',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 6, 7',
+		'win(union)' => '1, 3, 6, 7',
+		'unions($10)' => '1, 3, 5, 6, 7',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 1, 2, 11',
+		'place(end-favorites )' => '11',
+		'super sure bet' => 'super sure place 11',
+		'places($10)' => '11',
+		'sures($10)' => '11',
+		'super sures($10)' => '11',
+		'place(wp 628)' => '1',
+		'placesWP($30)' => '1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 1, 2, 3, 4, 7',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 4, 5, 6',
+		'win(union)' => '1, 4, 5, 6',
+		'unions($10)' => '1, 2, 4, 5, 6',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 5',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 1, 3, 9, 10',
+	],
+];
